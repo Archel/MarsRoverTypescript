898a43039f7adabe15643c8c60f6512f1f1698c5
Daniel Jordi Perez <danipq26@gmail.com>
2017-11-16 19:05:46 +0100

    Throws and invalid input if the input is invalid

--- src/CommandParser.ts
+++ src/CommandParser.ts
@@ -1,7 +1,21 @@
 import EmptyInputException from "./Errors/EmptyInputException";
+import InvalidInputException from "./Errors/InvalidInputException";
+
+const VALID_COMMAND_REGEX = /^[0-9]+ [0-9]+\\n[0-9]+ [0-9]+ [N|W|S|E]\\n[L|M|R]+/;
 
 export default class CommandParser {
     public parse(input: string): void {
-        throw new EmptyInputException();
+        if (input === "") {
+            throw new EmptyInputException();
+        }
+        
+        if (!this.isValid(input)) {
+            throw new InvalidInputException();
+        }
+        
+    }
+
+    private isValid(input: string): boolean {
+        return VALID_COMMAND_REGEX.test(input);
     }
 }
--- test/CommandParser.ts
+++ test/CommandParser.ts
@@ -1,5 +1,6 @@
 import CommandParser from "../src/CommandParser";
 import EmptyInputException from "../src/Errors/EmptyInputException";
+import InvalidInputException from "../src/Errors/InvalidInputException";
 
 describe("Command parser should", () => {
     it("throw an exception if the input is empty", () => {
@@ -7,5 +8,12 @@ describe("Command parser should", () => {
             const commandParser = new CommandParser();
             commandParser.parse("");
         }).toThrow(EmptyInputException);
+    });
+
+    it("throw an exception if the input is invalid", () => {
+        expect(() => {
+            const commandParser = new CommandParser();
+            commandParser.parse("asdfasdfasdfasdf");
+        }).toThrow(InvalidInputException);
     });
 });
